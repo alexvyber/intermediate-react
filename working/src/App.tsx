783608c396224d4f6df19725ce3b6ece757afc82
Alexey Sokolov <alexvyber@gmail.com>
2023-02-23 22:28:18 +0300

    finished 1-4 videos

--- working/src/App.tsx
+++ working/src/App.tsx
@@ -1,32 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom"
+import { Nav } from "./components/nav"
+import { Home } from "./components/home"
+import "./App.css"
+import { UseRef } from "./routes/use-ref"
+import { UseReducer } from "./routes/use-reducer"
+import { UseMemo } from "./routes/use-memo"
+import { UseCallback } from "./routes/use-callback"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      asdf
+      <Nav />
+      <br />
+      <br />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/useReducer" element={<UseReducer />} />
+        <Route path="/useMemo" element={<UseMemo />} />
+        <Route path="/useCallback" element={<UseCallback />} />
+        <Route path="/useRef" element={<UseRef />} />
+        {/* <Route path="/useLayoutEffect" element={<UseLayoutEffect />} /> */}
+        {/* <Route path="/useId" element={<UseId />} /> */}
+      </Routes>
     </div>
   )
 }
